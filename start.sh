df4026c2b99d319474cc41713bf224a916d204ce
ShervinChila <108692453+ShervinChila@users.noreply.github.com>
2022-08-05 21:47:55 +0430

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/ShervinChila/filenahai /ShervinChila
+  git clone https://github.com/ShervinChila/Eva /ShervinChila
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /ShervinChila
